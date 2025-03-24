7f70b1a1bc1f4aa46dccc998ec213fb94187f302
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2025-03-23 18:43:44 -0600

    change: change ddl  version 2.0.1

--- ddl.sql
+++ ddl.sql
@@ -23,18 +23,17 @@ CREATE TABLE Usuario (
         REFERENCES Rol(id_rol)
 );
 
--- FlujoAprobacion: Nota – se impone la regla que 'creado_por' < 'id_flujo'.
+
 CREATE TABLE FlujoAprobacion (
     id_flujo INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     descripcion TEXT,
     version_actual INT,
     es_plantilla BOOLEAN,
-    fecha_creacion TIMESTAMP,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     creado_por INT,
     CONSTRAINT fk_flujo_creado_por FOREIGN KEY (creado_por)
         REFERENCES Usuario(id_usuario),
-    CONSTRAINT chk_flujo_creado_por CHECK (creado_por < id_flujo)
 );
 
 -- PasoFlujo
@@ -43,13 +42,13 @@ CREATE TABLE PasoFlujo (
     flujo_id INT NOT NULL,
     nombre VARCHAR(255) NOT NULL,
     tipo_flujo VARCHAR(50) NOT NULL,   -- Valores esperados: 'normal', 'bifurcacion', 'union'
-    tipo_paso VARCHAR(50) NOT NULL,     -- Valores: 'Ejecucion', 'Aprobacion'
-    regla_aprobacion VARCHAR(50) NOT NULL,  -- Valores: 'Unanime', 'Individual', 'Ancla'
+    tipo_paso VARCHAR(50) NOT NULL,     -- Valores: 'ejecucion', 'aprobacion'
+    regla_aprobacion VARCHAR(50) NOT NULL,  -- Valores: 'unanime', 'individual', 'ancla'
     CONSTRAINT fk_paso_flujo FOREIGN KEY (flujo_id)
         REFERENCES FlujoAprobacion(id_flujo),
     CONSTRAINT chk_tipo_flujo CHECK (tipo_flujo IN ('normal', 'bifurcacion', 'union')),
-    CONSTRAINT chk_tipo_paso CHECK (tipo_paso IN ('Ejecucion', 'Aprobacion')),
-    CONSTRAINT chk_regla_aprobacion CHECK (regla_aprobacion IN ('Unanime', 'Individual', 'Ancla'))
+    CONSTRAINT chk_tipo_paso CHECK (tipo_paso IN ('ejecucion', 'aprobacion')),
+    CONSTRAINT chk_regla_aprobacion CHECK (regla_aprobacion IN ('unanime', 'individual', 'ancla'))
 );
 
 -- CaminoParalelo
@@ -67,14 +66,15 @@ CREATE TABLE CaminoParalelo (
 -- Solicitud
 CREATE TABLE Solicitud (
     id_solicitud INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    flujo_base_id INT NOT NULL,
+    flujo_base_id INT,
     solicitante_id INT NOT NULL,
-    fecha_creacion TIMESTAMP,
-    estado VARCHAR(50),
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    estado VARCHAR(50) , -- Valores: 'Aprobado', 'Rechazado'
     CONSTRAINT fk_solicitud_flujo FOREIGN KEY (flujo_base_id)
         REFERENCES FlujoAprobacion(id_flujo),
     CONSTRAINT fk_solicitud_solicitante FOREIGN KEY (solicitante_id)
         REFERENCES Usuario(id_usuario)
+    CONSTRAINT chk_solicitud_estado CHECK (estado IN ('aprobado', 'rechazado'))
 );
 
 -- FlujoActivo
@@ -82,13 +82,15 @@ CREATE TABLE FlujoActivo (
     id_flujo_activo INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     solicitud_id INT NOT NULL,
     flujo_ejecucion_id INT NOT NULL,
-    fecha_inicio TIMESTAMP,
+    fecha_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fecha_finalizacion TIMESTAMP,
-    estado VARCHAR(50),
+    estado VARCHAR(50), -- Valores: 'encurso', 'finalizado', 'cancelado'
     CONSTRAINT fk_flujoactivo_solicitud FOREIGN KEY (solicitud_id)
         REFERENCES Solicitud(id_solicitud),
     CONSTRAINT fk_flujoactivo_flujo FOREIGN KEY (flujo_ejecucion_id)
         REFERENCES FlujoAprobacion(id_flujo)
+    CONSTRAINT chk_flujoactivo_estado CHECK (estado IN ('encurso', 'finalizado', 'cancelado'))
+    CONSTRAINT chk_fechas CHECK (fecha_inicio < fecha_finalizacion)
 );
 
 -- RelacionVisualizador
@@ -105,24 +107,24 @@ CREATE TABLE RelacionVisualizador (
 -- Inputs
 CREATE TABLE Inputs (
     id_input INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    tipo_input VARCHAR(50) NOT NULL,  -- Valores: 'TextoCorto', 'TextoLargo', 'ComboBox', 'MultipleCheckbox', 'Date', 'Number', 'Archivo'
-    CONSTRAINT chk_tipo_input CHECK (tipo_input IN ('TextoCorto', 'TextoLargo', 'ComboBox', 'MultipleCheckbox', 'Date', 'Number', 'Archivo'))
+    tipo_input VARCHAR(50) NOT NULL,  -- Valores: 'textocorto', 'textolargo', 'combobox', 'multiplecheckbox', 'date', 'number', 'archivo'
+    CONSTRAINT chk_tipo_input CHECK (tipo_input IN ('textocorto', 'textolargo', 'combobox', 'multiplecheckbox', 'date', 'number', 'archivo'))
 );
 
 -- PasoSolicitud
 CREATE TABLE PasoSolicitud (
     id_paso_solicitud INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     flujo_activo_id INT NOT NULL,
-    paso_id INT,   -- Opcional para pasos agregados
+    paso_id INT,   -- Opcional para pasos que no vienen de plantilla
     camino_id INT NOT NULL,
     responsable_id INT,
-    fecha_inicio TIMESTAMP,
+    fecha_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fecha_fin TIMESTAMP,
-    tipo_paso VARCHAR(50) NOT NULL,   -- Valores: 'Ejecucion', 'Aprobacion'
-    estado VARCHAR(50),               -- Valores: 'Aprobado', 'Rechazado', 'Excepcion'
+    tipo_paso VARCHAR(50) NOT NULL,   -- Valores: 'ejecucion', 'aprobacion'
+    estado VARCHAR(50) ,               -- Valores: 'aprobado', 'rechazado', 'excepcion', 
     nombre VARCHAR(255),
     tipo_flujo VARCHAR(50) NOT NULL,    -- Valores: 'normal', 'bifurcacion', 'union'
-    regla_aprobacion VARCHAR(50) NOT NULL, -- Valores: 'Unanime', 'Individual', 'Ancla'
+    regla_aprobacion VARCHAR(50) NOT NULL, -- Valores: 'unanime', 'individual', 'incla'
     CONSTRAINT fk_pasosolicitud_flujoactivo FOREIGN KEY (flujo_activo_id)
         REFERENCES FlujoActivo(id_flujo_activo),
     CONSTRAINT fk_pasosolicitud_paso FOREIGN KEY (paso_id)
@@ -131,11 +133,11 @@ CREATE TABLE PasoSolicitud (
         REFERENCES CaminoParalelo(id_camino),
     CONSTRAINT fk_pasosolicitud_responsable FOREIGN KEY (responsable_id)
         REFERENCES Usuario(id_usuario),
-    CONSTRAINT chk_pasosolicitud_tipo_paso CHECK (tipo_paso IN ('Ejecucion', 'Aprobacion')),
-    CONSTRAINT chk_pasosolicitud_estado CHECK (estado IN ('Aprobado', 'Rechazado', 'Excepcion')),
+    CONSTRAINT chk_pasosolicitud_tipo_paso CHECK (tipo_paso IN ('ejecucion', 'aprobacion')),
+    CONSTRAINT chk_pasosolicitud_estado CHECK (estado IN ('aprobado', 'rechazado', 'excepcion')),
     CONSTRAINT chk_pasosolicitud_tipo_flujo CHECK (tipo_flujo IN ('normal', 'bifurcacion', 'union')),
-    CONSTRAINT chk_pasosolicitud_regla_aprobacion CHECK (regla_aprobacion IN ('Unanime', 'Individual', 'Ancla')),
-    CONSTRAINT chk_paso_vs_flujo CHECK (id_paso_solicitud < flujo_activo_id)
+    CONSTRAINT chk_pasosolicitud_regla_aprobacion CHECK (regla_aprobacion IN ('unanime', 'individual', 'ancla')),
+    CONSTRAINT chl_pasofechas CHECK (fecha_inicio < fecha_fin)
 );
 
 -- RelacionInput
@@ -210,6 +212,9 @@ CREATE TABLE Delegacion (
         REFERENCES Usuario(id_usuario),
     CONSTRAINT fk_delegacion_grupo FOREIGN KEY (grupo_aprobacion_id)
         REFERENCES GrupoAprobacion(id_grupo)
+    CONSTRAINT chk_delegacion_fechas CHECK (
+         fecha_Inicio < fecha_Fin
+    )
 );
 
 -- RelacionUsuarioGrupo
@@ -239,7 +244,8 @@ CREATE TABLE Comentario (
     CONSTRAINT fk_comentario_usuario FOREIGN KEY (usuario_id)
         REFERENCES Usuario(id_usuario),
     CONSTRAINT chk_comentario_exclusivo CHECK (
-         (paso_solicitud_id IS NOT NULL OR flujo_activo_id IS NOT NULL)
+            (paso_solicitud_id IS NOT NULL AND flujo_activo_id IS NULL)
+            OR (paso_solicitud_id IS NULL AND flujo_activo_id IS NOT NULL)
     )
 );
 
@@ -248,11 +254,12 @@ CREATE TABLE Notificacion (
     id_notificacion INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     usuario_id INT NOT NULL,
     mensaje TEXT,
-    prioridad VARCHAR(50),
+    prioridad VARCHAR(50), -- Valores: 'baja', 'media', 'alta', 'critica'
     leida BOOLEAN,
-    fecha_envio TIMESTAMP,
+    fecha_envio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_notificacion_usuario FOREIGN KEY (usuario_id)
         REFERENCES Usuario(id_usuario)
+    CONSTRAINT chk_notificacion_prioridad CHECK (prioridad IN ('baja', 'media', 'alta', 'critica'))
 );
 
 -- Propuesta
@@ -262,8 +269,7 @@ CREATE TABLE Propuesta (
     descripcion TEXT,
     usuario_creador_id INT NOT NULL,
     flujo_id INT,  -- Opcional
-    estado VARCHAR(50),
-    fecha_creacion TIMESTAMP,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT fk_propuesta_usuario FOREIGN KEY (usuario_creador_id)
         REFERENCES Usuario(id_usuario),
     CONSTRAINT fk_propuesta_flujo FOREIGN KEY (flujo_id)
@@ -274,11 +280,11 @@ CREATE TABLE Propuesta (
 CREATE TABLE Votacion (
     id_votacion INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     propuesta_id INT NOT NULL,
-    tipo VARCHAR(50),
-    resultado VARCHAR(50),
+    resultado VARCHAR(50), -- Valores: 'aprobado', 'rechazado'
     fecha_cierre TIMESTAMP,
     CONSTRAINT fk_votacion_propuesta FOREIGN KEY (propuesta_id)
         REFERENCES Propuesta(id_propuesta)
+    CONSTRAINT chk_votacion_resultado CHECK (resultado IN ('aprobado', 'rechazado'))
 );
 
 -- Voto
@@ -286,22 +292,23 @@ CREATE TABLE Voto (
     id_voto INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     votacion_id INT NOT NULL,
     usuario_id INT NOT NULL,
-    valor VARCHAR(50),
+    valor VARCHAR(50), -- Valores: 'aprobado', 'rechazado'
     fecha TIMESTAMP,
     CONSTRAINT fk_voto_votacion FOREIGN KEY (votacion_id)
         REFERENCES Votacion(id_votacion),
     CONSTRAINT fk_voto_usuario FOREIGN KEY (usuario_id)
         REFERENCES Usuario(id_usuario)
+    CONSTRAINT chk_voto_valor CHECK (valor IN ('aprobado', 'rechazado'))
 );
 
 -- Backup
 CREATE TABLE Backup (
     id_backup INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     fecha TIMESTAMP,
-    tipo VARCHAR(50),           -- Ej.: 'completo', 'incremental'
-    ubicacion VARCHAR(255),
-    tipo_contenido VARCHAR(50),  -- 'archivo' o 'enlace'
-    referencia_contenido VARCHAR(255),
+    tipo VARCHAR(50) CHECK (tipo IN ('completo', 'incremental', 'diferencial', 'espejo')),
+    ubicacion VARCHAR(255), -- Opcional: podría ser el bucket o región de S3
+    tipo_contenido VARCHAR(50) CHECK (tipo_contenido IN ('archivo', 'enlace')),
+    referencia_contenido VARCHAR(255) NOT NULL, -- Obligatorio si siempre hay un enlace o archivo
     usuario_id INT NOT NULL,
     CONSTRAINT fk_backup_usuario FOREIGN KEY (usuario_id)
         REFERENCES Usuario(id_usuario)
@@ -311,8 +318,8 @@ CREATE TABLE Backup (
 CREATE TABLE Incidente (
     id_incidente INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     descripcion TEXT,
-    severidad VARCHAR(50),
-    estado VARCHAR(50),
+    severidad VARCHAR(50) CHECK (severidad IN ('baja', 'media', 'alta', 'crítica')),
+    estado VARCHAR(50) CHECK (estado IN ('enrevision', 'resuelto', 'cerrado')),
     usuario_reporta_id INT NOT NULL,
     fecha_reporte TIMESTAMP,
     CONSTRAINT fk_incidente_usuario FOREIGN KEY (usuario_reporta_id)
@@ -327,9 +334,9 @@ CREATE TABLE Metrica (
     flujo_id INT NOT NULL,
     fecha_calculo TIMESTAMP,
     descripcion TEXT,
-    unidad VARCHAR(50),
+    unidad VARCHAR(50) CHECK (unidad IN ('segundos', 'minutos', 'horas', 'porcentaje', 'número')),
     meta FLOAT,
-    tipo_metrica VARCHAR(50),
+    tipo_metrica VARCHAR(50) CHECK (tipo_metrica IN ('productividad', 'calidad', 'eficiencia')),
     CONSTRAINT fk_metrica_flujo FOREIGN KEY (flujo_id)
         REFERENCES FlujoAprobacion(id_flujo)
 );
@@ -338,7 +345,7 @@ CREATE TABLE Metrica (
 CREATE TABLE Informe (
     id_informe INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nombre VARCHAR(255),
-    tipo VARCHAR(50),          -- Ej.: 'resumen', 'detallado'
+    tipo VARCHAR(50) CHECK (tipo IN ('resumen', 'detallado', 'comparativo', 'auditoría')),
     fecha_generacion TIMESTAMP,
     usuario_generador_id INT NOT NULL,
     contenido TEXT,
